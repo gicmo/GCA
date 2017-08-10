f776108bb49a87ffbcf928d3bfdfc54c5313cbc4
Achilleas Koutsou <achilleas.k@gmail.com>
2017-08-10 14:37:30 +0200

    Embed map data

--- Tools/embedinfo.py
+++ Tools/embedinfo.py
@@ -5,13 +5,15 @@ import json
 
 if len(sys.argv) < 4:
     print("Embeds info and schedule into the conference info.json")
-    print("\n\tUsage: embedinfo.py info.json info.md schedule.json")
+    print("\n\tUsage: embedinfo.py info.json info.md schedule.json maps.json")
     print()
     sys.exit(1)
 
-infojson, infomd, schedjson = sys.argv[1:]
+infojson, infomd, schedjson, mapjson = sys.argv[1:]
 
-print("Embeding {} and {} into {}".format(infomd, schedjson, infojson))
+print("Embeding\n\t{}\n\t{}\n\t{}\ninto\n\t{}\n".format(
+    infomd, schedjson, mapjson, infojson
+))
 
 with open(infomd) as fp:
     infopage = "".join(fp.readlines())
@@ -20,11 +22,16 @@ with open(schedjson) as fp:
     sched = "".join(fp.readlines())
     sched = sched.replace('"', '\"')
 
+with open(mapjson) as fp:
+    maps = "".join(fp.readlines())
+    maps = maps.replace('"', '\"')
+
 with open(infojson) as fp:
     confinfo = json.load(fp)
 
 confinfo["info"] = infopage
 confinfo["schedule"] = sched
+confinfo["geo"] = maps
 
 
 infofname, infoext = os.path.splitext(infojson)
