ffb4bfee83d31a4a1377fb3830b3d49436498619
Christian Kellner <christian@kellner.me>
2022-08-23 10:57:57 +0200

    WIP

---
+++ Source/ConferenceCell.swift
@@ -0,0 +1,24 @@
+//
+//  ConferenceCell.swift
+//  GCA
+//
+//  Created by Christian Kellner on 09.08.22.
+//  Copyright © 2022 G-Node. All rights reserved.
+//
+
+import UIKit
+
+class ConferenceCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
